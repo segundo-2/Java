335660ec5f126933d14d88d4cf2dd6e9a0dad81d
TooEasy4You <hamed5677646@gmail.com>
2024-07-31 17:57:03 +0400

    tt

--- src/Main.java
+++ src/Main.java
@@ -4,29 +4,12 @@ public class Main {
 
 
 
+        for(int i = 0; i <= 5; i++){
 
-
-    }
-
-    // Empty
-    public static void myOwnFunction(){
-
-    }
-
-    // Accept 2 int
-    public static void myFunction(int num1, int num2){
-
-        System.out.println(num1 + num2);
-
-
-    }
-
-    // Accept 2 String
-    public static void myFun(String name1, String name2) {
-
-    }
-    public static void My(String s1, int nmu){
-
+            System.out.println(i);
         }
+
+
+    }
 
 }
