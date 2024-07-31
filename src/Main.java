f0668ffc6036d40534a76b33e99823fc9e08d7d8
TooEasy4You <hamed5677646@gmail.com>
2024-07-31 17:18:10 +0400

    aa

--- src/Main.java
+++ src/Main.java
@@ -2,41 +2,10 @@
 public class Main {
     public static void main(String[] args) {
 
-            int nmu = 77;
-            int num ;
-            double dob = 7.5;
-            double dob1;
-            boolean f;
 
-            boolean t= true ;
-            String txt = "Hello";
-            String tt;
-        System.out.println("Welcome everybody");
-        System.out.println("Here we have a integer "+nmu);
-        System.out.println(dob+" is a double number");
-
-        if(nmu >= 50){
-
-            System.out.println("Hello there");
-
-        }else if(nmu <= 50){
-            System.out.println("Hi there");
-
-        }else{
-            System.out.println("no one is here");
-        }
+        String text = "Hello world";
 
 
-        if(dob>=7.1){
-            System.out.println("the number is greater than 7.1");
-
-        } else if (dob==7.1) {
-            System.out.println("the number equal 7.1");
-
-        }
-        else{
-            System.out.println("the number less than 7.1");
-        }
 
     }
 }
