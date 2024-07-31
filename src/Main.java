2eff240795fb167f140e825cbb087862415a7b52
segundo-2 <styx.367@gmail.com>
2024-07-31 17:25:35 +0400

    aa

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,19 @@ public class Main {
 
 
         String text = "Hello world";
+        String txt1= "";
+         if(text.isEmpty()){
 
+         }
+        else{
+             System.out.println(text);
+         }
 
-
+        if(txt1.isEmpty()){
+            System.out.println("Empty");
+        }
+        else{
+            System.out.println(txt1);
+        }
     }
 }
