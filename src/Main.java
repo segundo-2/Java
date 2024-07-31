68ed2409f0d7ed984254e48b2eee8dcdc3cc6092
TooEasy4You <hamed5677646@gmail.com>
2024-07-31 18:11:04 +0400

    tt

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,9 @@
 
 public class Main {
     public static void main(String[] args) {
-        /*for(int i = 0; i <= 5; i++){
 
-            System.out.println(i);
-        }*/
-        for(int i=0;i<=20;i++){
-            if(i%2==0){
-                System.out.println(i);
-            }
-        }
+
+
 
     }
 
