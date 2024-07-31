dc9feb654be01a91115a2b8386350e92bd04319f
segundo-2 <styx.367@gmail.com>
2024-07-31 18:23:10 +0400

    aa

--- src/Loly.java
+++ src/Loly.java
@@ -2,6 +2,9 @@ public class Loly {
     public void Func(int n1, int n2){
         System.out.println(n1*n2);
     }
-    public static void Loly{
-    Func(1,2);
-}}
+    Loly anyName = new Loly()
+    public void Loly{
+        anyName.Func(1,2);
+
+}
+}
