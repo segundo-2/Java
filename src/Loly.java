5e19db8805068850c1f7ea6ec9c4eb0d27053d57
TooEasy4You <hamed5677646@gmail.com>
2024-08-01 16:12:17 +0400

    tt

--- src/Loly.java
+++ src/Loly.java
@@ -1,18 +1,7 @@
-import java.util.Scanner;
+
 
 public class Loly {
 
-
-    public void Func(int n1, int n2){
-
-        Scanner something = new Scanner(System.in);
-
-        int test;
-
-        System.out.println("enter your age");
-        test = something.nextInt();
-        System.out.println("your age is " +test);
-    }
 
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,7 @@
-import java.sql.SQLOutput;
-import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
-        Scanner something = new Scanner(System.in);
-        int age;
-        System.out.println("Enter your age: ");
-        age= something.nextInt();
-        System.out.println(age);
-
-        // to call the class
-        Loly anything = new Loly();
-
-        // to call the function
-        anything.Func(2,3);
-
-
 
 
     }
