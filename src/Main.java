459a41b0a5bf02b6c117f5446d671cf3befec362
segundo-2 <styx.367@gmail.com>
2024-08-01 16:19:33 +0400

    aa

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,22 @@
-
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner something = new Scanner(System.in);
+        int n;
+        double d;
+        String txt;
+        System.out.println("Enter number: ");
+        n=something.nextInt();
+        System.out.println("Enter a double number: ");
+        d=something.nextDouble();
+        System.out.println("Enter any text: ");
+        txt=something.next();
+        System.out.println("The number you enterd is "+n);
+        System.out.println("The double number you enterd is "+d);
+        System.out.println("The text you enterd is "+txt);
+
+
 
 
     }
