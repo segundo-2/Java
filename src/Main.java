6c54aefa7a7ecd6e68c5e88e9ab82d15799eb9ca
segundo-2 <styx.367@gmail.com>
2024-08-01 16:05:45 +0400

    aa

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
         Scanner something = new Scanner(System.in);
+        int age;
+        System.out.println("Enter your age: ");
+        age= something.nextInt();
+        System.out.println(age);
+
         // to call the class
         Loly anything = new Loly();
 
